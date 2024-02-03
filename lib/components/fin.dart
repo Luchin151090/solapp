4b1c341e0cac3ae19e067dbfe771da5242237a1a
josemiguel-AAA <joseeeeagua.sol@gmail.com>
2024-02-03 11:42:26 -0500

    fin y formulario responsivos

--- lib/components/fin.dart
+++ lib/components/fin.dart
@@ -1,5 +1,5 @@
 /*
-import 'package:app_final/components/test/hola.dart';
+import 'package:appsol_final/components/hola.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:lottie/lottie.dart';
@@ -15,7 +15,8 @@ class _FinState extends State<Fin> {
   @override
   Widget build(BuildContext context) {
     //final TabController _tabController = TabController(length: 2, vsync: this);
-
+    final anchoActual = MediaQuery.of(context).size.width;
+    final largoActual = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 255, 255, 255),
         body: PopScope(
@@ -37,49 +38,49 @@ class _FinState extends State<Fin> {
                         margin: const EdgeInsets.only(top: 0,left: 20),
                         child:const Text("Gracias por",
                         style: TextStyle(
-                          color:Colors.black,fontWeight: FontWeight.w300,fontSize: 30),),
+                          color:Colors.black,fontWeight: FontWeight.w300,fontSize: largoActual * 0.04),),
                       ),
                       Container(
                         margin: const EdgeInsets.only(left: 20),
                         child: const Text("Llevar vida a tu HOGAR!",
-                        style: TextStyle(fontSize: 30),),
+                        style: TextStyle(fontSize: largoActual * 0.04),),
                       ),
                        Container(
                         margin: const EdgeInsets.only(left: 20),
                         child: const Text("con",
-                        style: TextStyle(fontSize: 35),),
+                        style: TextStyle(fontSize: largoActual * 0.047),),
                       ),
                        Container(
                         margin: const EdgeInsets.only(left: 20),
                         child: Row(
                           children: [
-                            const Text("Agua Sol",
+                            Text("Agua Sol",
                             style: TextStyle(
-                              fontSize: 50,
+                              fontSize: largoActual * 0.068,
                               ),),
                               Container(
-                                height: 100,
-                                width: 100,
+                                height: largoActual * 0.13,
+                                width: largoActual * 0.13,
                                 
                                 child: Lottie.asset('lib/imagenes/arboles.json'))
                           ],
                         ),
                       ),
-                      const SizedBox(height: 20,),
+                      SizedBox(height: largoActual * 0.027,),
                       Container(
                         margin: const EdgeInsets.only(left:30),
                         //color: Colors.grey,
-                        width: 100,
-                        height: 100,
+                        width: largoActual * 0.13,
+                        height: largoActual * 0.13,
                         child: Image.asset('lib/imagenes/logo_sol_tiny.png').animate().fade(delay:1500.ms ).then().shake(),
                       ),
-                      const SizedBox(height: 20,),
+                      SizedBox(height: largoActual * 0.027,),
           
                       Container(
                         margin: const EdgeInsets.only(left: 20),
-                        height: 60,
+                        height: largoActual * 0.081,
                         //color:Colors.grey,
-                        width: 140,
+                        width: anchoActual * 0.39,
                         child: ElevatedButton(onPressed: (){
                           Navigator.push(
                         context,
@@ -90,16 +91,16 @@ class _FinState extends State<Fin> {
                         style: ButtonStyle(
                           backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 1, 34, 60))
                         ),
-                         child:const Text("<< Menú",style: TextStyle(fontSize: 20,color:Colors.white),),
+                         child:const Text("<< Menú",style: TextStyle(fontSize: largoActual * 0.027,color:Colors.white),),
                          ),
           
                       ),
-                      const SizedBox(height: 20,),
+                      SizedBox(height: largoActual * 0.027,),
                       Expanded(
                         child: Container(
                           //color:Colors.grey,
-                           width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height/1.75,
+                           width: anchoActual,
+                          height: largoActual/1.75,
                           child: Image.asset('lib/imagenes/reparte.png'),
                         ),
                       ),
